643a33c8a418d73f85f079389d87c51ca3cfe908
Michael Michlin <mmulya@outlook.com>
2024-03-09 01:23:45 -0500

    Continue with types

--- src/api/BasicTypes.ts
+++ src/api/BasicTypes.ts
@@ -1,8 +1,59 @@
-import {
-    PropType, MultiLink, ModelClassName, Class, AModel, AClass
-} from "./ModelTypes";
+/**
+ * Represents language-specific scalar types used to work with class properties.
+ */
+export type LangType = string | number| bigint | boolean ;
 
 
+
+/**
+ * Represents data types corresponding to string property types:
+ *   - "str" - string
+ *   - "clob" - character-based large object
+ */
+export type StringDataType = "str" | "clob";
+
+/**
+ * Represents data types corresponding to temporal property types:
+ *   - "date" - date only
+ *   - "time" - time only
+ *   - "datetime" - datetime
+ *   - "timestamp" - timestamp
+ */
+export type TemporalDataType = "date" | "time" | "datetime" | "timestamp";
+
+/**
+ * Represents underlying data types corresponding to integer property types:
+ *   - "int" - signed or unsigned integer of different sizes
+ *   - "bigint" - signed or unsigned integer of different sizes
+ *   - "bit" - bit-values
+ *   - "year" - 4-digit year
+ */
+export type IntegerDataType = "int" | "bigint" | "bit" | "year";
+
+/**
+ * Represents underlying data types corresponding to real (floating or fixed point) property types:
+ *   - "real" - floating-point numbers of single or double precision
+ *   - "dec" - fixed-point numbers (DECIMAL/NUMERIC)
+ */
+export type RealDataType = "real" | "dec"
+
+/**
+ * Represents underlying data types corresponding to big integer property types:
+ *   - "bigint" - signed or unsigned integer of different sizes
+ *   - "bit" - bit-values
+ */
+export type BigintDataType = "bigint" | "bit";
+
+/**
+ * Combines integer and real data types in one definition for convenience
+ */
+export type NumericDataType = IntegerDataType | RealDataType
+
+/**
+ * Represents underlying data types corresponding to Boolean property types:
+ *   - "bool" - boolean
+ */
+export type BoolDataType ="bool";
 
 /**
  * Represents underlying data types corresponding to property types:
@@ -17,13 +68,10 @@ import {
  *   - "int" - signed or unsigned integer of different sizes
  *   - "real" - floating-point numbers of single or double precision
  *   - "dec" - fixed-point numbers (DECIMAL/NUMERIC)
- *   - "bit" - bit-values
- *   - "timestamp" - timestamp
+ *   - "year" - 4-digit year
  * - bigint properties
  *   - "bigint" - signed or unsigned integer of different sizes
- *   - "dec" - fixed-point numbers (DECIMAL/NUMERIC)
  *   - "bit" - bit-values
- *   - "timestamp" - timestamp
  * - boolean properties
  *   - "bool" - boolean
  * - Special properties
@@ -33,27 +81,36 @@ import {
  *   - "arr" - array
  */
 export type DataType =
-    "str" | "clob" |
-    "bool" |
-    "int" | "bigint" | "real" | "dec" | "bits" |
-    "date" | "time" | "datetime" | "timestamp" |
-    "link" | "multilink" |
-    "obj" | "arr";
+    StringDataType | NumericDataType | TemporalDataType | BoolDataType |
+    "link" | "multilink" | "obj" | "arr";
+
+
+
+// /**
+//  * Represents underlying data type corresponding to the given property type
+//  */
+// export type DataTypeOfPropType<M extends AModel, T extends PropType> =
+//     T extends string ? "str" | "clob" | "date" | "time" | "datetime" | "timestamp" :
+//     T extends number ? "int" | "real" | "dec" | "year" :
+//     T extends bigint ? "bigint" | "bit" :
+//     T extends boolean ? "bool" :
+//     T extends MultiLink<AClass> ? "multilink" :
+//     T extends Class<infer CN,any,any> ? CN extends ModelClassName<M>
+//         ? "link"
+//         : never :
+//     never
 
 
 
 /**
- * Represents underlying data type corresponding to the given property type
+ * Maps data types to language types used to represent them.
  */
-export type DataTypeOfPropType<M extends AModel, T extends PropType> =
-    T extends string ? "str" | "date" | "time" | "datetime" | "timestamp" :
-    T extends number ? "int" | "real" | "dec" | "bit" | "ts" :
-    T extends bigint ? "bigint" | "dec" | "bit" | "ts" :
-    T extends boolean ? "bool" :
-    // T extends Date ? "timestamp" :
-    T extends MultiLink<AClass> ? "multilink" :
-    T extends Class<infer CN,any,any> ? CN extends ModelClassName<M>
-        ? "link"
-        : never :
-    never
+export type LangTypeOf<DT extends DataType> =
+    DT extends StringDataType | TemporalDataType ? string :
+    DT extends BigintDataType ? bigint :
+    DT extends NumericDataType ? number :
+    DT extends BoolDataType ? boolean :
+    object;
+
+
 
--- src/api/ExpressionTypes.ts
+++ src/api/ExpressionTypes.ts
@@ -1,53 +1,82 @@
-export type TermKind = "lit" | "var" | "op" | "func";
-
-export type LangDataType = string | number | bigint | boolean;
-
-export type TermDataType = "string" | "int" | "real" | "decimal" | "bool" | "datetime";
-
-export type LangToTerm<T extends LangDataType> =
-    T extends string ? "string" | "datetime" :
-    T extends number ? "int" | "real" | "decimal" :
-    T extends bigint ? "int" :
-    T extends boolean ? "bool" :
-    never;
-
-export type TermToLang<DT extends TermDataType> =
-    DT extends "string" | "datetime" ? string :
-    DT extends "int" | "real" | "decimal" ? number | bigint :
-    DT extends "bool" ? boolean :
-    never;
+import { DataType, LangTypeOf, NumericDataType, StringDataType } from "./BasicTypes";
 
 
 
-export type TermDataTypeFuncSignature = (...args: TermDataType[]) => TermDataType;
+/**
+ * Kinds of expressions:
+ * - lit - wraps a literal value passed as a language-specific type with indication of the data type.
+ * - var - wraps a "variable"
+ * - func - function invocation
+ * - op - arithmetic or logical operation. They are very similar to function invocation; however
+ *   operations have different precedence rules.
+ */
+export type ExprKind = "lit" | "var" | "func" | "op";
 
-type IStringFuncs =
+
+
+/**
+ * Function signature expressed via DataType types (instead of language types). This is used
+ * to represent data types of parameters and return values for operations and functions that can
+ * be invoked on expressions.
+ */
+export type DataTypeFunc = (...args: DataType[]) => DataType;
+
+/**
+ * Tuple that defines to what data types (specified as a first element) the given function
+ * (specified as a seconf element) can apply - that is, can be invoked on as a method.
+ */
+export type DataTypeMethod = [DataType, DataTypeFunc]
+
+/**
+ * Represents functions and opertions that can be invoked on expression of the given data type.
+ */
+export interface IFunctionsAndOperations
 {
-    substr: (start: "int") => "string"
-    match: (pattern: "string") => "bool"
-    concat: (...args: "string"[]) => "string"
+    // string functions
+    substr: [StringDataType, (start: "int") => StringDataType]
+    match: [StringDataType, (pattern: "str") => "bool"]
+    concat: [StringDataType, (...args: "str"[]) => StringDataType]
+
+    // numeric functions
+    abs: [NumericDataType, () => NumericDataType]
+    pow: [NumericDataType, (exp: "int") => NumericDataType]
+    minus: [NumericDataType, (...args: NumericDataType[]) => NumericDataType]
 }
 
-type INumberFuncs =
-{
-    pow: (exp: "int") => "int"
-}
-
-
-export type MethodsOfInterface<I extends Record<string,TermDataTypeFuncSignature>> =
-    { [op in string & keyof I as `$${op}`]-?: I[op] extends TermDataTypeFuncSignature ? LangDataTypeFuncSugnature<I[op]> : never }
-
-export type Expr<DT extends TermDataType> =
-    {} &
-    (
-        DT extends "string" ? MethodsOfInterface<IStringFuncs> :
-        DT extends "int" ? MethodsOfInterface<INumberFuncs> :
-        {}
-    )
+/**
+ * Helper type that from all the methods in the IFunctionsAndOperations interface, selects only
+ * those that can be applied to the given data type.
+ *
+ * @typeParam DT DataType for which methods should be selected
+ */
+export type LangMethodsOf<DT extends DataType> =
+    { [N in string & keyof IFunctionsAndOperations as DT extends IFunctionsAndOperations[N][0] ? N : never]-?:
+        IFunctionsAndOperations[N] extends DataTypeMethod
+            ? LangTypeFunc<IFunctionsAndOperations[N][1]> : never }
 
 
 
-export type TermBase<K extends TermKind, DT extends TermDataType> = Expr<DT> &
+/**
+ * Represents an expression of the given DataType.
+ *
+ * @typeParam DT DataType detrmining what methods and operations can be invoked on the expression.
+ */
+export type Expr<DT extends DataType> =
+    { [op in string & keyof LangMethodsOf<DT> as `$${op}`]-?: LangMethodsOf<DT>[op] }
+
+
+
+export type MappedParamsTuple<T extends DataType[]> = { [i in keyof T]: LangTypeOf<T[i]> | Expr<T[i]> };
+
+export type FuncParametersTuple<F extends (...args: DataType[]) => DataType> =
+    MappedParamsTuple<Parameters<F>>;
+
+export type LangTypeFunc<F extends (...args: DataType[]) => DataType> =
+    (...args: FuncParametersTuple<F> extends any[] ?FuncParametersTuple<F> : any[]) => Expr<ReturnType<F>>
+
+
+
+export type TermBase<K extends ExprKind, DT extends DataType> = Expr<DT> &
     {
         kind: K;
         dt: DT;
@@ -55,24 +84,14 @@ export type TermBase<K extends TermKind, DT extends TermDataType> = Expr<DT> &
 
 
 
-export type MappedParamsTuple<T extends TermDataType[]> = { [i in keyof T]: TermToLang<T[i]> | Expr<T[i]> };
-
-export type FuncParametersTuple<F extends (...args: TermDataType[]) => TermDataType> =
-    MappedParamsTuple<Parameters<F>>;
-
-export type LangDataTypeFuncSugnature<F extends (...args: TermDataType[]) => TermDataType> =
-    (...args: MappedParamsTuple<Parameters<F>> extends any[] ? MappedParamsTuple<Parameters<F>> : any[]) => Expr<ReturnType<F>>
-
-
-
-export type LiteralTerm<DT extends TermDataType> = TermBase<"lit", DT> &
+export type LiteralTerm<DT extends DataType> = TermBase<"lit", DT> &
     {
-        v: TermToLang<DT>;
+        v: LangTypeOf<DT>;
     }
 
 
 
-export type FuncTerm<FN extends string, F extends (...args: TermDataType[]) => TermDataType> =
+export type FuncTerm<FN extends string, F extends (...args: DataType[]) => DataType> =
     TermBase<"func", ReturnType<F>> &
     {
         /** Function name */
@@ -84,27 +103,29 @@ export type FuncTerm<FN extends string, F extends (...args: TermDataType[]) => T
 
 
 
-type Term2 = FuncTerm<"foo", (s: "string", start: "int", x: "bool") => "string">;
+type Term2 = FuncTerm<"foo", (s: "str", start: "int", x: "bool") => "str">;
 type FirstParam2 = Term2["args"][0];
 let fp2: FirstParam2;
 type SecondParam2 = Term2["args"][1];
 let sp2: SecondParam2;
 type ThirdParam2 = Term2["args"][2];
 let tp2: ThirdParam2;
-let term2: Term2 = {} as Term2;
-term2.$substr(1).$match("abc")
+let term2 = {} as Term2;
+term2.$substr(1).$concat("a", "b", "c").$match("abc")
 
-type Term3 = FuncTerm<"and", (...args: "bool"[]) => "bool">;
+type Term3 = FuncTerm<"and", (...args: "bool"[]) => "int">;
 type FirstParam3 = Term3["args"][0];
 let fp3: FirstParam3;
 type SecondParam3 = Term3["args"][1];
 let sp3: SecondParam3;
+let term3 = {} as Term3;
+let res = term3.$minus(1, 2, 3).$abs();
 
 type Term4 = FuncTerm<"and", () => "bool">;
 // @ts-expect-error - no element at index 0
 type FirstParam4 = Term4["args"][0];
 
-type Term5 = FuncTerm<"and", (first: "string", ...args: "bool"[]) => "bool">;
+type Term5 = FuncTerm<"and", (first: "str", ...args: "bool"[]) => "bool">;
 type FirstParam5 = Term5["args"][0];
 let fp5: FirstParam5;
 type SecondParam5 = Term5["args"][1];
